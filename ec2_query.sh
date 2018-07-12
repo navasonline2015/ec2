c9a22b616b0749255eb6c6912eb2bc9e6b6579ff
Muhammed Navas <NAVASONLINE@GMAIL.COM>
2018-07-12 02:57:16 +0000

    Edited

--- ec2_query.sh
+++ ec2_query.sh
@@ -1,26 +1,16 @@
 #!/usr/bin/bash
-if [ "$#" -eq 0 ]; then
-	echo "Error: you missed number of instances. usage : $0 <NUMBER>" 
-fi
-ex_resources=$(aws ec2 describe-instances | jq -r '.Reservations[].Instances[]|[.ImageId,.VpcId,.KeyName,.SecurityGroups[].GroupId]'| grep  -v '\[\|\]'|awk -F ',' '{print $1}')
-i="1"
-while [ "$i" -le 4 ]
+IFS=$'\n'
+printf "choose from below, Use space if you want to display more Properties (Eg: 1 3 5) :"
+#choice=read
+echo ""
+echo "1. Display Instance Ids"
+echo "2. Display Public Dns Name"
+echo "3. Display Public Ip Address"
+echo "4. Display Private Ip Address"
+echo "5. Display Vpc IDs"
+echo "6. Display Image IDs"
+echo "7. Display Instance Status"
+for i in `aws ec2 describe-instances | jq -r '.Reservations[].Instances[]| {Instance_Id: .InstanceId,Public_Dns: .PublicDnsName,Public_IP: .PublicIpAddress,Private_Ip: .PrivateIpAddress,Vpc: .VpcId,Image_Id: .ImageId,Status: .State.Name}'`
 do
- for resource in $ex_resources;
-  do
-     if [ "$i" -eq 1 ]; then
-        ImageId="$resource"
-    elif [ "$i" -eq 2 ]; then
-        VpcId="$resource"
-    elif [ "$i" -eq 3 ]; then
-        KeyName="$resource"
-    elif [ "$i" -eq 4 ]; then
-        Sec_Group="$resource"
-    else
-	:
-    fi
-    i=$[$i+1]
- done
+	echo "$i"| cut -d "," -f1 | awk -F '"' {'print $2,$3,$4'}  |grep "Instance_Id"
 done
-exec `aws ec2 run-instances  --image-id $ImageId  --security-group-ids $Sec_Group  --count $1 --instance-type t2.micro  --key-name $KeyName`
-
