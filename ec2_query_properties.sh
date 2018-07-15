f073dd4498ddced9cd5f55883142b170d9de4003
Muhammed Navas <NAVASONLINE@GMAIL.COM>
2018-07-15 20:38:55 +0000

    small query

--- ec2_query_properties.sh
+++ ec2_query_properties.sh
@@ -15,110 +15,49 @@ printf "choose from above, Use space if you want to display more Properties (Eg:
 read choice
 
 howmany() { echo $#; }
-string1='InstanceId:.InstanceId'
-string2='Public_Dns:.PublicDnsName'
-string3='Public_IP:.PublicIpAddress' 
-string4='Private_Ip:.PrivateIpAddress'
-string5='Vpc:.VpcId'
-string6='Image_Id:.ImageId'
-string7='Status:.State.Name'
-string8='SubnetId:.SubnetId'
-string9='InstanceType:.InstanceType'
-string10="$string1,$string2,$string3,$string4,$string5,$string6,$string7,$string8,$string9"
 n=1
-
 for i in $choice;
-   do
+do
+	if [ $i == 10 ] && [ `howmany $choice` == 1 ];
+	then
+   	  echo `aws ec2 describe-instances | jq -r -C ".Reservations[].Instances[]|{InstanceId:.InstanceId,Public_Dns:.PublicDnsName,Public_IP:.PublicIpAddress,Private_Ip:.PrivateIpAddress,Vpc:.VpcId,Image_Id:.ImageId,Status:.State.Name,SubnetId:.SubnetId,InstanceType:.InstanceType}"| grep -v '{}'`
+	else
+  		if [ $i == 1 ]; then
+    		  string1='InstanceId:.InstanceId'
+         	  string=$string1
+  		elif [ $i == 2 ]; then
+    		  string2='Public_Dns:.PublicDnsName'
+    		  string=$string2
+  		elif [ $i == 3 ]; then
+    		  string3='Public_IP:.PublicIpAddress'
+    		  string=$string3
+  		elif [ $i == 4 ]; then
+    		  string4='Private_Ip:.PrivateIpAddress'
+    		  string=$string4
+ 		elif [ $i == 5 ]; then
+    		  string5='Vpc:.VpcId'
+    		  string=$string5
+ 		elif [ $i == 6 ]; then
+    		  string6='Image_Id:.ImageId'
+    		  string=$string6
+		 elif [ $i == 7 ]; then
+    		  string7='Status:.State.Name'
+    		  string=$string7
+ 		elif [ $i == 8 ]; then
+    		  string8='SubnetId:.SubnetId'
+    		  string=$string8
+ 		elif [ $i == 9 ]; then
+    		 string9='InstanceType:.InstanceType'
+    		 string=$string9
+ 		fi 
 	
-        if [ $i == 1 ];then
-           if [ `howmany $choice` == 1 ]; then
-               command_string+="$string1"
-	   elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-		command_string+=",$string1"
-	   elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-		command_string+="$string1"
-           fi 
-        elif [ $i == 2 ];then   
-           if [ `howmany $choice` == 1 ];then
-               command_string+="$string2"
-           elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-                command_string+=",$string2"
-           elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-                command_string+="$string2"
-           fi
-
-        elif [ $i == 3 ];then
-           if [ `howmany $choice` == 1 ];then
-               command_string+="$string3"
-           elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-                command_string+=",$string3"
-           elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-                command_string+="$string3"
-           fi
-
-	elif [ $i == 4 ];then
-           if [ `howmany $choice` == 1 ];then
-               command_string+="$string4"
-           elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-                command_string+=",$string4"
-           elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-                command_string+="$string4"
-           fi
-
-	elif [ $i == 5 ];then
-           if [ `howmany $choice` == 1 ];then
-               command_string+="$string5"
-           elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-                command_string+=",$string5"
-           elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-                command_string+="$string5"
-           fi
-
-	elif [ $i == 6 ];then
-           if [ `howmany $choice` == 1 ];then
-               command_string+="$string6"
-           elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-                command_string+=",$string6"
-           elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-                command_string+="$string6"
-           fi
-
-	elif [ $i == 7 ];then
-           if [ `howmany $choice` == 1 ];then
-               command_string+="$string7"
-           elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-                command_string+=",$string7"
-           elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-                command_string+="$string7"
-           fi
-	elif [ $i == 8 ];then
-           if [ `howmany $choice` == 1 ];then
-               command_string+="$string8"
-           elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-                command_string+=",$string8"
-           elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-                command_string+="$string8"
-           fi
-	elif [ $i == 9 ];then
-           if [ `howmany $choice` == 1 ];then
-               command_string+="$string9"
-           elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-                command_string+=",$string9"
-           elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-                command_string+="$string9"
-           fi
-	elif [ $i == 10 ];then
-           if [ `howmany $choice` == 1 ];then
-               command_string+="$string10"
-           elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
-                command_string+=",$string10"
-           elif [ `howmany $choice` > 1 ] && [ $n == 1 ];then
-                command_string+="$string9"
-           fi
-
-        fi
-
-
-   ((n++))
- done    
+ 		if [ `howmany $choice` == 1 ] || [ `howmany $choice` > 1 ] && [ $n == 1 ]; then
+                  command_string+="$string"
+		
+		elif [ `howmany $choice` > 1 ] && [ $n != 1 ];then
+		  command_string+=",$string"
+		fi
+	fi   
+ ((n++))
+done
 echo `aws ec2 describe-instances | jq -r -C ".Reservations[].Instances[]|{$command_string}"`
